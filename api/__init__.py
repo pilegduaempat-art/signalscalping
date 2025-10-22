b0a5ae5b21c3ea1d813b454025fdfdb7fd6ecf16
pilegduaempat-art <pilegduaempat@gmail.com>
2025-10-22 22:26:19 +0700

    Create __init__.py for API module
    Initialize API module with functions from Binance and Coinglass.

---
+++ api/__init__.py
@@ -0,0 +1,23 @@
+# api/__init__.py
+"""
+API modules for external services
+"""
+from .binance import (
+    fetch_exchange_info,
+    fetch_24hr_ticker,
+    fetch_klines,
+    fetch_funding_rate,
+    fetch_open_interest,
+    fetch_agg_trades
+)
+from .coinglass import fetch_coinglass_metric
+
+__all__ = [
+    'fetch_exchange_info',
+    'fetch_24hr_ticker',
+    'fetch_klines',
+    'fetch_funding_rate',
+    'fetch_open_interest',
+    'fetch_agg_trades',
+    'fetch_coinglass_metric'
+]
